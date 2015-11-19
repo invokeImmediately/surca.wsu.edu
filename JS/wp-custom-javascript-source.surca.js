a4227c6dc9758ed42dc480e0010d5989f19e0385
Daniel Rieck <danielcrieck@gmail.com>
2015-11-19 15:33:34 -0800

    Updated script file name

--- JS/wp-custom-javascript-source.surca.js
+++ JS/wp-custom-javascript-source.surca.js
@@ -1,6 +1,6 @@
 // See [https://github.com/invokeImmediately/surca.wsu.edu] for repository of source code
 (function($) {
-    $.ajax({ url: '/wp-content/uploads/sites/706/2015/11/surca-custom.js', dataType: 'script', cache: true });
+    $.ajax({ url: '/wp-content/uploads/sites/706/2015/11/surca-custom1.js', dataType: 'script', cache: true });
     $.ajax({ url: '/wp-content/uploads/sites/706/2015/11/jQuery.textResize.js', dataType: 'script', cache: true });
     ($('.has-tool-tip').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/706/2015/11/jQuery.qTip_.js', dataType: 'script', cache: true }) : 0;
     ($('.page-covering-notice-js').length !== 0) ? $.ajax({ url: '/wp-content/uploads/sites/706/2015/11/jQuery.cookieObjs.js', dataType: 'script', cache: true }) : 0;
