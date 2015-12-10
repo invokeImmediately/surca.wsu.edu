cf28288d976215ce88dc8b8f87c997efd0747624
Daniel Rieck <danielcrieck@gmail.com>
2015-12-09 17:00:10 -0800

    Added node.js based build file for custom javascript source code

---
+++ JS/build-production-file.js
@@ -0,0 +1,12 @@
+/* NODE.JS - Build Production JavaScript File */
+var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
+concat([
+ './surca-custom.js',
+ '../WSU-UE---JS/jQuery.textResize.js',
+ '../WSU-UE---JS/jQuery.qTip.js',
+ '../WSU-UE---JS/jQuery.cookieObjs.js',
+ '../WSU-UE---JS/jquery.media.js',
+ '../WSU-UE---JS/jQuery.autoScrollingImages.js'
+ ], './wp-custom-js-source.js', function() {
+    console.log('Concatenation complete.');     
+ });
