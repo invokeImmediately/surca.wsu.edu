04588455de6028b1d4e7d944ef515258c923b263
Daniel Rieck <danielcrieck@gmail.com>
2016-03-24 08:43:29 -0700

    Added new file to build queue

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -2,6 +2,7 @@
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
  './surca-custom.js',
+ '../WSU-UE---JS/jQuery.oue-custom.js',
  '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.cookieObjs.js',
