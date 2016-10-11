8478be628896f3959bcd4f7ac02ec7fa2a097b4f
Daniel Rieck <danielcrieck@gmail.com>
2016-10-11 12:30:13 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -394,6 +394,326 @@ function isJQuery($obj) {
     });
 })(jQuery);
 // 14.4px;
+/************************************************************************************************************\
+| JQUERY-MEDIATED ENHANCED INTERACTIVITY OF GRAVITY FORM FIELDS                                              |
+\************************************************************************************************************/
+(function ($) {
+    "use strict";
+    
+	$(document).bind("gform_post_render", function () {
+		var $rqrdFlds =  $("li.gfield_contains_required");
+		checkRqrdInpts($rqrdFlds.find("input"));
+		checkRqrdChckbxs($rqrdFlds.find("ul.gfield_checkbox, ul.gfield_radio"));
+		checkRqrdTxtAreas($rqrdFlds.find("textarea"));
+	});
+	$(document).ready(function () {
+        if($("div.gform_body").length > 0) {
+			initWsuIdInputs(".gf-is-wsu-id");
+            setupActvtrChckbxs(".oue-gf-actvtr-checkbox");
+            setupActvtrChain(".oue-gf-actvtr-chain");
+            setupUploadChain(".oue-gf-upload-chain");
+			
+            // TODO: streamline functions by querying all ul.gform_fields li.gfield, then determine 
+            //   how to handle object by finding div children with gfield_container_class.
+			var $rqrdFlds =  $("li.gfield_contains_required");
+			hghlghtRqrdInpts($rqrdFlds.find("input"));
+			hghlghtRqrdChckbxs($rqrdFlds.find("ul.gfield_checkbox, ul.gfield_radio"));
+			hghlghtRqrdTxtAreas($rqrdFlds.find("textarea"));
+			hghlghtRqrdSelects($rqrdFlds.find("select"));
+        }
+    });
+	$(window).load(function () {
+		hghlghtRqrdRchTxtEdtrs($("li.gfield_contains_required.uses-rich-editor"));
+	});
+    
+    /****************************************************************************************************\
+    | Highlight required INPUTS until a value has been properly entered                                  |
+    \****************************************************************************************************/
+    function checkRqrdInpts ($fields) {
+        if (isJQuery($fields)) {
+            $fields.each(function () {
+                var $thisInput = $(this);
+				if ($thisInput.val() == "") {
+					$thisInput.removeClass("gf-value-entered");
+				}
+				else {
+					$thisInput.addClass("gf-value-entered");
+				}
+            });
+        }
+    }
+	
+    function hghlghtRqrdInpts ($fields) {
+        if (isJQuery($fields)) {
+            $fields.each(function () {
+                var $thisInput = $(this);
+				$thisInput.blur(function () {
+					if ($thisInput.val() == "") {
+						$thisInput.removeClass("gf-value-entered");
+					}
+					else {
+						$thisInput.addClass("gf-value-entered");
+					}
+				});
+            });
+        }
+    }
+
+    /****************************************************************************************************\
+    | Highlight required CHECKBOXES until at least one has been checked                                  |
+    \****************************************************************************************************/
+    function checkRqrdChckbxs ($fields) {
+        if (isJQuery($fields)) {
+            $fields.each(function () {
+                var $this = $(this);
+                var $inputs = $this.find("input");
+				var inputReady = false;
+                $inputs.each(function () {
+					if ($(this).prop("checked") == true && !inputReady) {
+						inputReady = true;
+					}
+				});
+				if (inputReady) {
+					$this.addClass("gf-value-entered");
+				}
+				else {
+					$this.removeClass("gf-value-entered");
+				}
+			});
+		}
+	}
+
+    function hghlghtRqrdChckbxs ($fields) {
+        if (isJQuery($fields)) {
+            $fields.each(function () {
+                var $this = $(this);
+                var $inputs = $this.find("input");
+                $inputs.each(function () {
+                    var $thisChild = $(this);
+                    $thisChild.change(function () {
+                        var $thisParent, $parentsInputs;
+                        var inputReady = false;
+                        
+                        $thisParent = $thisChild.parents("ul.gfield_checkbox, ul.gfield_radio");
+                        $parentsInputs = $thisParent.find("input");
+                        $parentsInputs.each(function () {
+                            if ($(this).prop("checked") == true && !inputReady) {
+                                inputReady = true;
+                            }
+                        });
+                        if (inputReady) {
+                            $thisParent.addClass("gf-value-entered");
+                        }
+                        else {
+                            $thisParent.removeClass("gf-value-entered");
+                        }
+                    });
+                });
+            });
+        }
+    }
+
+    /****************************************************************************************************\
+    | Highlight required TEXT AREA inputs until a value has been properly entered                        |
+    \****************************************************************************************************/
+    function checkRqrdTxtAreas ($fields) {
+		checkRqrdInpts($fields);
+    }
+
+    function hghlghtRqrdTxtAreas ($fields) {
+		hghlghtRqrdInpts($fields);
+    }
+
+    /****************************************************************************************************\
+    | Highlight required RICH TEXT EDITOR containters until a value has been properly entered            |
+    \****************************************************************************************************/
+	function hghlghtRqrdRchTxtEdtrs($fields) {
+        if (isJQuery($fields) && $fields.length > 0) {
+            $fields.each(function () {
+				var $edtrFrm = $(this).find("iframe");
+				$edtrFrm.each(function () {
+					var $edtrBdy = $(this).contents().find("#tinymce");
+					$edtrBdy.css("background-color", "rgba(255,0,0,0.1)");
+					$edtrBdy.focus(function () {
+						$(this).css("background-color", "rgba(255,255,255,1)");
+					});
+					$edtrBdy.blur(function () {
+						var $this = $(this);
+						if($this.text().replace(/\n|\uFEFF/g, "") == "") {
+							$this.css("background-color","rgba(255,0,0,0.1)");
+						}
+					});
+				});
+			});
+		}
+	}
+
+    /****************************************************************************************************\
+    | Highlight required SELECTS until at least one has been checked                                     |
+    \****************************************************************************************************/
+    function hghlghtRqrdSelects ($fields) {
+        if (isJQuery($fields)) {
+            $fields.each(function () {
+                var $thisInput = $(this);
+				var $childSlctdOptn = $thisInput.find("option:selected");
+				var optionVal = $childSlctdOptn.text();                        
+				if (optionVal != "") {
+					$thisInput.addClass("gf-value-entered");
+				}
+				else {
+					$thisInput.removeClass("gf-value-entered");
+				}
+				$thisInput.change(function () {
+					$childSlctdOptn = $thisInput.find("option:selected");
+					optionVal = $childSlctdOptn.text();                        
+					if (optionVal != "") {
+						$thisInput.addClass("gf-value-entered");
+					}
+					else {
+						$thisInput.removeClass("gf-value-entered");
+					}
+				});
+            });
+        }
+    }
+
+    /****************************************************************************************************\
+    | Initialize RegEx filtration of inputs that accept WSU ID numbers                                   |
+    \****************************************************************************************************/
+    function initWsuIdInputs(slctrInputs) {
+        var $wsuIdInputs = $(slctrInputs).find("input[type='text']");
+        $wsuIdInputs.on("keyup paste", function () {
+            var $this = $(this);
+            var regExMask = /[^0-9]+/g;
+            var inputText = $this.val();
+            if (regExMask.exec(inputText) != null) {
+                $this.val(inputText.replace(regExMask, ""));
+                inputText = $this.val();
+				alert("WSU ID numbers can only contain digts.");
+            }
+            if (inputText.length > 9) {
+                $this.val(inputText.slice(0,9));
+				alert("WSU ID numbers are no greater than nine (9) digits in length.");
+            }
+        });
+        $wsuIdInputs.blur(function () {
+            var $this = $(this);
+            var regExFinalPttrn = /(?:^[0-9]{8}$)|(?:^0[0-9]{8}$)/;
+            var inputText = $this.val();
+			if (inputText != "") {
+				if (regExFinalPttrn.exec(inputText) == null) {					
+					$this.val("");
+					alert("Please try again: when the leading zero is included, WSU ID numbers are nine (9) digits long. (You can also drop the leading zero and enter in eight (8) digits.");
+				}
+			}
+        });
+    }
+	
+    /****************************************************************************************************\
+    | Setup activator checkboxes that disappear once one is selected                                     |
+    \****************************************************************************************************/
+    function setupActvtrChckbxs (selector) {
+        if ($.type(selector) === "string") {
+            $(".gform_body").on("change", selector + " input", function () {
+                var $thisChild = $(this);
+                var $thisParent = $thisChild.parents(selector);
+                $thisParent.addClass("gf-activated");
+            });
+        }
+    }
+    
+    /****************************************************************************************************\
+    | Setup a chain of activator checkboxes, wherein once a checkbox is activated/deactivated,           |
+    | only its closest previous sibling is hidden/shown.                                                 |
+    \****************************************************************************************************/
+    function setupActvtrChain (selector) {
+        if ($.type(selector) === "string") {
+            $(".gform_body").on("change", selector + " input", function () {
+                var $thisChild = $(this);
+                var $thisParent = $thisChild.parents(selector);
+                var $parentPrevSblngs = $thisParent.prevAll(selector);
+                if($thisChild.prop("checked")) {
+                    $parentPrevSblngs.first().addClass("gf-hidden");
+                }
+                else {
+                    $parentPrevSblngs.first().removeClass("gf-hidden");
+                }
+            });
+        }
+    }
+
+    /****************************************************************************************************\
+    | Setup a chain of file uploading inputs, wherein only the left-most input in the tree is            |
+    | visible. As the user uploads files in sequence, the next nearest neighbor is unveiled.             |
+    \****************************************************************************************************/
+    function setupUploadChain (selector) {
+        if ($.type(selector) === "string") {
+            /* CHECK IF UPLOADS ALREADY EXIST:
+             *  It is possible to arrive at this point in execution after the user has submitted a
+             *  form containing errors that also already contains transcripts uploaded to input
+             *  fields that will be hidden by default. The following blocks of code resolve this
+             *  situation by showing such fields, as well as their nearest neighbors.
+             */
+            var $inputs = $(selector + " input[type='file']");
+            $inputs.each(function () {
+                var $thisInput = $(this);
+                var $nextDiv = $thisInput.nextAll("div[id]").first();
+                if($nextDiv.length > 0) {
+                    $thisInput.addClass("gf-value-entered");
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    $parentOfInput.removeClass("gf-hidden");
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector).first();
+                    $parentNextSblngs.removeClass("gf-hidden");
+                }
+            });
+            $(".gform_body").on("change", selector + " input[type='file']", function () {
+                var $thisInput = $(this);
+                if($thisInput.prop("files") != null && $thisInput.prop("files").length > 0) {
+                    var valuePassed = true;
+                    var $parentOfInput = $thisInput.parents(selector).first();
+                    var $parentNextSblngs = $parentOfInput.nextAll(selector);
+                    var $parentPrevSblngs = $parentOfInput.prevAll(selector);
+                    if($parentNextSblngs.length != 0 || $parentPrevSblngs.length != 0) {
+                        var originalFileName = $thisInput.prop("files").item(0).name;
+                        $parentPrevSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                        $parentNextSblngs.each(function () {
+                            if(valuePassed) {
+                                var $thisSblng = $(this);
+                                var $thisSblngInput = $thisSblng.find("input[type='file']").first();
+                                if($thisSblngInput.prop("files") != null && $thisSblngInput.prop("files").length > 0) {
+                                    var thisFileName = $thisSblngInput.prop("files").item(0).name;
+                                    valuePassed = originalFileName != thisFileName;
+                                }
+                            }
+                        });
+                    }
+                    if(valuePassed) {                      
+                        $thisInput.addClass("gf-value-entered");
+                        $parentNextSblngs.first().removeClass("gf-hidden");
+                    }
+                    else
+                    {
+                        alert("A file with the same name has already been uploaded; please choose a different file.");
+                        $thisInput.get(0).value = "";
+                    }
+                }
+                else {
+                    $thisChild.removeClass("gf-value-entered");
+                }
+            });
+        }
+    }
+    
+ })(jQuery);
 /**********************************************************************************************************************
  JQUERY QTIP TOOL TIPS PLUGIN
  *********************************************************************************************************************/
