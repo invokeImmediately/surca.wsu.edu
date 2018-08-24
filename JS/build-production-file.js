88390f5283f76034c92a15bdcd6b09503383f96d
Daniel Rieck <danielcrieck@gmail.com>
2018-08-24 15:48:40 -0700

    Modify build sequence

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -5,11 +5,14 @@ concat([
  '../WSU-UE---JS/jQuery.oue-custom.js',
  '../WSU-UE---JS/jQuery.textResize.js',
  '../WSU-UE---JS/jQuery.forms.js',
+ '../../jquery.AreYouSure/jquery.are-you-sure.js',
  '../WSU-UE---JS/jquery.are-you-sure.js',
+ '../../qTip2/dist/jquery.qtip.min.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.cookieObjs.js',
  '../WSU-UE---JS/jquery.media.js',
- '../WSU-UE---JS/jQuery.countdown.min.js'
+ '../../jQuery.countdown/dist/jquery.countdown.min.js',
+ '../WSU-UE---JS/jQuery.countdown-custom.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
  });
