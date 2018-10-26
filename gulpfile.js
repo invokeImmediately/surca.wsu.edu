0292b280415a51acc5c0158282918ea8d25d6192
Daniel Rieck <danielcrieck@gmail.com>
2018-10-26 14:40:43 -0700

    Add gulpfile, dependencies for making CSS, JS builds

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/* -------------------------------------------------------------------------------------------------
+** Variable Declarations
+*/
+
+// Gulp task dependencies
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+
+/* -------------------------------------------------------------------------------------------------
+** Function declarations
+*/
+
+/**
+ * Get the settings for a gulp-mediated custom CSS build from Less source files.
+ *
+ * @return {object} - Instance of gulpBuilder.CssBuildSettings.
+ */
+function getCssBuildSettings() {
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
+Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
+	var insertingMediaQuerySectionHeader = {
+			'before': /^@media/,
+			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
+════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
+################################################################################# ║\r\n*   ╚═══════\
+═══════════════════════════════════════════════════════════════════════════════════════════════════\
+═════════════╝\r\n*/',
+			'stopAfterFirstMatch': true
+		};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+se see [https://github.com/invokeImmediately/surca.wsu.edu] for a repository of source code. */\r\n\
+';
+ 	var sourceFile = './CSS/surca-custom.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
+ 		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+ 		minCssFileHeaderStr, sourceFile);
+}
+
+/**
+ * Get the settings for a gulp-mediated custom JS build.
+ *
+ * @return {object} - Simple collection of settings for JS builds.
+ */
+function getJsBuildSettings() {
+	return {
+		buildDependenciesList: [
+			'./WSU-UE---JS/jQuery.oue-custom.js',
+			'./WSU-UE---JS/jQuery.textResize.js',
+			'./WSU-UE---JS/jQuery.forms.js',
+			'../jQuery.AreYouSure/jquery.are-you-sure.js',
+			'./WSU-UE---JS/jQuery.are-you-sure.js',
+			'../qTip2/dist/jquery.qtip.min.js',
+			'./WSU-UE---JS/jQuery.qTip.js',
+			'./WSU-UE---JS/jQuery.cookieObjs.js',
+			'./WSU-UE---JS/jquery.media.js',
+			'../jQuery.countdown/dist/jquery.countdown.min.js',
+			'./WSU-UE---JS/jQuery.countdown-custom.js'
+			'../imagesloaded/imagesloaded.pkgd.min.js',
+			'../masonry/dist/masonry.pkgd.min.js',
+			'./WSU-UE---JS/jQuery.masonry-custom.js',
+			'./JS/surca-custom.js'
+		],
+		commentNeedle: /^(\/\*)(?!!)/g,
+		compiledJsFileName: 'surca-build.js',
+		destFolder: './JS/',
+		minJsFileExtension: '.min.js',
+		replaceCallback: fixFileHeaderComments
+	};
+}
+
+/* -------------------------------------------------------------------------------------------------
+** Main execution sequence
+*/
+
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
