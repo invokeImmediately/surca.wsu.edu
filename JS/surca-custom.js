b2b91d5222962d8d4a3679f8d87bb792a6d5cf37
invokeImmediately <danielcrieck@gmail.com>
2019-11-13 16:01:40 -0800

    Modify process for inserting headers into news pages
    Update the markup for the header to be inserted, and add header to more news related pages than the primary news feed

--- JS/surca-custom.js
+++ JS/surca-custom.js
@@ -1,17 +1,47 @@
-/**********************************************************************************************************************
- CUSTOM JQUERY-BASED DYNAMIC CONTENT
- *********************************************************************************************************************/
-(function ($) {
+/*!*************************************************************************************************
+ * surca-custom.js
+ * -------------------------------------------------------------------------------------------------
+ * SUMMARY: Custom JS code used specifically on the WSU SURCA website.
+ *
+ * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ *
+ * REPOSITORY: https://github.com/invokeImmediately/WSU-UE---JS
+ *
+ * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
+ *
+ *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ *   without fee is hereby granted, provided that the above copyright notice and this permission
+ *   notice appear in all copies.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+ *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *   PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************************************/
+
+( function ( $ ) {
     "use strict";
-	$(document).ready(function () {
-			/**********************************************************************************************
-			 * Tweak HTML source to work around some quirks of the WordPress setup                        *
-			 **********************************************************************************************/
-			var surcaSiteURL = window.location.pathname;
-			switch(surcaSiteURL) {
-				case '/news/':
-					$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-					break;
-			}
-	});
-})(jQuery);
+
+	$( function () {
+		insertHeaderIntoNewsPages( '<section class="row single gutter pad-top"><div class="column o\
+ne"><section class="article-header header-attending mw--80ch" style="padding-top: 0;"><div class="h\
+eader-content"><h1 class="auto-fits-text data-min-fs-28 data-resize-against-article-header"><span c\
+lass="title">News<span class="for-screen-readers">:</span></span> <span class="subtitle auto-fits-t\
+ext data-min-fs-18 data-resize-against-article-header">What We and Our Students Have Accomplished</\
+span></h1></div></section>' );
+	} );
+
+	function insertHeaderIntoNewsPages( htmlInserted ) {
+		var relUrl = window.location.pathname;
+		if ( relUrl == '/news/' ) {
+			$('div.column.one').first().parent('section').before( htmlInserted );
+		} else {
+			var $body = $( 'body' ).first();
+			if ( $body.hasClass( 'single-post' ) || $body.hasClass( 'archive' ) ) {
+				$body.find( '.column.one' ).first().parent( '.row' ).before( htmlInserted );
+			}			
+		}
+	}
+} )( jQuery );
