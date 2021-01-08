c1011ebaeed42970962261e2950545010e8549c6
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2021-01-08 15:44:00 -0800

    Fix bug in application of news page headers to web pages
    Fix a bug wherein the page header meant for application to pages in the news section of the website was being applied to the calendar page.

--- JS/surca-custom.js
+++ JS/surca-custom.js
@@ -1,47 +1,100 @@
 /*!*************************************************************************************************
- * surca-custom.js
+ * ▄▀▀▀ █  █ █▀▀▄ ▄▀▀▀ ▄▀▀▄    ▄▀▀▀ █  █ ▄▀▀▀▐▀█▀▌▄▀▀▄ ▐▀▄▀▌      █ ▄▀▀▀
+ * ▀▀▀█ █  █ █▄▄▀ █    █▄▄█ ▀▀ █    █  █ ▀▀▀█  █  █  █ █ ▀ ▌   ▄  █ ▀▀▀█
+ * ▀▀▀   ▀▀  ▀  ▀▄ ▀▀▀ █  ▀     ▀▀▀  ▀▀  ▀▀▀   █   ▀▀  █   ▀ ▀ ▀▄▄█ ▀▀▀ 
  * -------------------------------------------------------------------------------------------------
- * SUMMARY: Custom JS code used specifically on the WSU SURCA website.
+ * Custom JS code specfically written for use on the WSU SURCA website. The code is meant to be
+ *   applied to the site via the Custom JavaScript Editor page in the WordPress dashboard.
  *
- * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
- *
- * REPOSITORY: https://github.com/invokeImmediately/WSU-UE---JS
- *
- * LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.
- *
- *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
- *   without fee is hereby granted, provided that the above copyright notice and this permission
- *   notice appear in all copies.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
- *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
- *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
- *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
- *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
- *   PERFORMANCE OF THIS SOFTWARE.
+ * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
+ * @link https://github.com/invokeImmediately/surca.wsu.edu/blob/master/JS/surca-custom.js
+ * @license MIT - Copyright (c) 2020 Washington State University
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *     and associated documentation files (the “Software”), to deal in the Software without
+ *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ *     distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+ *     the Software is furnished to do so, subject to the following conditions:
+ *   The above copyright notice and this permission notice shall be included in all copies or
+ *     substantial portions of the Software.
+ *   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ *     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  **************************************************************************************************/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// TABLE OF CONTENTS
+// -----------------
+//   §1: Function declarations................................................................37
+//   §2: Main execution section...............................................................81
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ( function ( $ ) {
     "use strict";
 
-	$( function () {
-		insertHeaderIntoNewsPages( '<section class="row single gutter pad-top"><div class="column o\
-ne"><section class="article-header header-attending mw--80ch" style="padding-top: 0;"><div class="h\
-eader-content"><h1 class="auto-fits-text data-min-fs-28 data-resize-against-article-header"><span c\
-lass="title">News<span class="for-screen-readers">:</span></span> <span class="subtitle auto-fits-t\
-ext data-min-fs-18 data-resize-against-article-header">What We and Our Students Have Accomplished</\
-span></h1></div></section>' );
-	} );
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §1: Function Declarations
 
-	function insertHeaderIntoNewsPages( htmlInserted ) {
-		var relUrl = window.location.pathname;
-		if ( relUrl == '/news/' ) {
-			$('div.column.one').first().parent('section').before( htmlInserted );
-		} else {
-			var $body = $( 'body' ).first();
-			if ( $body.hasClass( 'single-post' ) || $body.hasClass( 'archive' ) ) {
-				$body.find( '.column.one' ).first().parent( '.row' ).before( htmlInserted );
-			}			
-		}
+function addPageHeaderOnCalendarPages( params ) {
+	let headerMarkup = params.htmlCalendarHeader;
+	addCalendarHeaderViaClassUtilization( headerMarkup );
+}
+
+/**
+ * Add page headers to news pages.
+ *
+ * @param {String} htmlNewsHeader - The HTML comprising the page header to be added to the DOM.
+ */
+function addPageHeaderOnNewsPages( params ) {
+	var headerMarkup = params.htmlNewsHeader;
+	addNewsHeaderViaLocation( headerMarkup );
+	addNewsHeaderViaClassUtilization( headerMarkup );
+}
+
+/**
+ * Inspect the body tag to add a header to news pages when certain classes are in use.
+ *
+ * @param {String} markup - The HTML comprising the page header to be added to the DOM.
+ */
+function addNewsHeaderViaClassUtilization( markup ) {
+	let $body = $( 'body' ).first();
+	if ( $body.hasClass( 'single-post' ) || ( $body.hasClass( 'archive' ) &&
+			( $body.hasClass( 'category' ) ||  $body.hasClass( 'tag' ) ) ) ) {
+		$body.find( '.column.one' ).first().parent( '.row' ).before( markup );
 	}
+}
+
+/**
+ * Use the browser's location to add a header to news pages, which lack them by default.
+ *
+ * @param {String} markup - The HTML comprising the page header to be added to the DOM.
+ */
+function addNewsHeaderViaLocation( markup ) {
+	var siteURL = window.location.pathname;
+	if ( siteURL == '/news/' ) {
+		$( '.column.one' ).first().parent( '.row' ).before( markup );
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Main execution section
+
+$( function () {
+	addPageHeaderOnNewsPages( {
+		htmlNewsHeader: '<section class="row single gutter pad-top">\n' +
+			'\t<div class="column one">' +
+			'\t\t<section class="article-header header-attending mw--80ch" style="padding-top: 0;">' +
+			'\t\t\t<div class="header-content">\n' +
+			'\t\t\t\t<h1 class="auto-fits-text data-min-fs-28 data-resize-against-article-header">' +
+			'<span class="title">News<span class="for-screen-readers">:</span></span> ' +
+			'<span class="subtitle auto-fits-text data-min-fs-18 data-resize-against-article-header">' +
+			'What We and Our Students Have Accomplished</span></h1>\n' +
+			'\t\t\t</div>\n' +
+			'\t\t</section>\n' +
+			'\t</div>\n' +
+			'</section>'
+	} );
+} );
+
 } )( jQuery );
