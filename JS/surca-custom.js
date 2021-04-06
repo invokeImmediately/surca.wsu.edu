c21b7c6353d3cb61a846d668a5f7ea89bd3100f1
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2021-04-06 15:07:04 -0700

    Enhancement: Add ProjectFinder to JS/surca-custom.js
    In JS/surca-custom.js, add the class ProjectFinder, which automatically discovers and functionalizes searchable presentation lists on the SURCA website so that each project's display card can be selected and viewed as a modal.

--- JS/surca-custom.js
+++ JS/surca-custom.js
@@ -3,14 +3,14 @@
  * ▀▀▀█ █  █ █▄▄▀ █    █▄▄█ ▀▀ █    █  █ ▀▀▀█  █  █  █ █ ▀ ▌   ▄  █ ▀▀▀█
  * ▀▀▀   ▀▀  ▀  ▀▄ ▀▀▀ █  ▀     ▀▀▀  ▀▀  ▀▀▀   █   ▀▀  █   ▀ ▀ ▀▄▄█ ▀▀▀ 
  *
- * @version 1.0.0
+ * @version 1.0.0-rc1
  *
  * Custom JS code specfically written for use on the WSU SURCA website. The code is meant to be
  *   applied to the site via the Custom JavaScript Editor page in the WordPress dashboard.
  *
  * @author Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
  * @link https://github.com/invokeImmediately/surca.wsu.edu/blob/master/JS/surca-custom.js
- * @license MIT - Copyright (c) 2020 Washington State University
+ * @license MIT - Copyright (c) 2021 Washington State University
  *   Permission is hereby granted, free of charge, to any person obtaining a copy of this software
  *     and associated documentation files (the “Software”), to deal in the Software without
  *     restriction, including without limitation the rights to use, copy, modify, merge, publish,
@@ -28,15 +28,71 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // TABLE OF CONTENTS
 // -----------------
-//   §1: Function declarations................................................................37
-//   §2: Main execution section..............................................................100
+//   §1: Class Declarations...................................................................37
+//   §2: Function Declarations................................................................37
+//   §3: Execution Entry Point...............................................................100
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ( function ( $ ) {
     "use strict";
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §1: Function Declarations
+// §1: Class Declarations
+
+class ProjectFinder {
+	// TODO: Implement the actions below through an object-oriented approach.
+	// Check for the project finder interface.
+	// If found, add the modal cover element to the page.
+	// Set up the dynamic behaviors of the project viewing buttons.
+
+	constructor( params ) {
+		this.selIntf = params.selIntf;
+		this.modalTrigger = params.modalTrigger;
+		this.abstTrigger = params.abstTrigger;
+		this.checkForIntf();
+	}
+
+	checkForIntf() {
+		this.$abstIntf = $( this.selIntf );
+		const proceed = this.$abstIntf.length === 1;
+		if ( proceed ) {
+			this.initialize();
+		}
+	}
+
+	initialize() {
+		// First, obtain a copy of a reference to the instance that will preserve lexical context in
+		//   handlers and callbacks.
+		const inst = this;
+
+		// Find the column that contains the project finder.
+		this.$intfCol = this.$abstIntf.parents( '.column.one' );
+
+		// Create a cover that will be shown when abstract cards are viewed by the user and thus need to
+		//   behave like a modal.
+		this.$modalCover = $( '<div class="modal-cover"></div>' );
+		this.$modalCover.prependTo( this.$intfCol );
+		this.$modalCover.on( 'click', function( e ) {
+			const $openAbst = $( '.abst-intf .expanded' );
+			$openAbst.toggleClass( inst.abstTrigger );
+			inst.$modalCover.toggleClass( inst.modalTrigger );
+		} );
+		
+		// Find all of the title buttons for the project.
+		this.$titleBtns = this.$abstIntf.find( '.project-title' );
+
+		// Attach event handlers to the title buttons so the abstract can be viewed.
+		this.$abstIntf.on( 'click', '.project-title', function( e ) {
+			const $btn = $( this );
+			const $abst = $btn.parent().parent();
+			$abst.toggleClass( inst.abstTrigger );
+			inst.$modalCover.toggleClass( inst.modalTrigger );
+		} );
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// §2: Function Declarations
 
 /**
  * Inspect the body tag to add a header to news pages when certain classes are in use.
@@ -98,8 +154,19 @@ function addPageHeaderOnNewsPages( params ) {
 	addNewsHeaderViaClassUtilization( headerMarkup );
 }
 
+/**
+ * Look for and set up the project finder interface on SURCA website pages as needed.
+ *
+ * @param {object} params - Simple object contained arguments for the function.
+ * @param {string} params.selIntf - A selector string that can be used by jQuery to fetch the
+ *   element representing the container of the project finder.
+ */
+function setUpProjectFinder( params ) {
+	const projFinder = new ProjectFinder( params );
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// §2: Main execution section
+// §3: Execution Entry Point
 
 $( function () {
 
@@ -133,6 +200,12 @@ $( function () {
 			'\t\t	<h1 class="tt--uppercase">News</h1>\n' +
 			'\t</div>\n' +
 			'</section>'
+	} );
+
+	setUpProjectFinder( {
+		selIntf: '.abst-intf',
+		modalTrigger: 'engaged',
+		abstTrigger: 'expanded'
 	} );
 } );
 
